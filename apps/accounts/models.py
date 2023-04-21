87df6d6fe618c827df38c836d1c232399e7c5576
Mykhailo Yakubets <mikhail.yakubets@gmail.com>
2023-04-21 11:58:36 +0300

    Add Account model to `accounts` app

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,1 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class Account(AbstractUser):
+    """
+    Main user model in the project.
+    """
+
+    last_activity = models.DateTimeField(
+        'User last request', auto_now_add=True, blank=True)
+
+    class Meta:
+        db_table = 'account'

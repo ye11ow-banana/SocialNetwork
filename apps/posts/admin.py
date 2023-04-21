b621bb7ac97a91332759f23d8d6c35d4f128dd9b
Mykhailo Yakubets <mikhail.yakubets@gmail.com>
2023-04-21 14:04:55 +0300

    Add admin to `posts` app

--- apps/posts/admin.py
+++ apps/posts/admin.py
@@ -1,1 +1,35 @@
 from django.contrib import admin
+
+from .models import Post, Media, PostLike
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'date_created')
+    search_fields = ('author', 'date_created')  # todo: check date search
+    list_filter = ('author', 'date_created')
+    readonly_fields = ('date_created',)
+    save_on_top = True
+    save_as = True
+
+
+@admin.register(Media)
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'media_type', 'post')
+    search_fields = ('title', 'post')
+    list_filter = ('title', 'media_type', 'post')
+    save_on_top = True
+    save_as = True
+
+
+@admin.register(PostLike)
+class PostLikeAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author')
+    search_fields = ('post', 'author')
+    list_filter = ('post', 'author')
+    save_on_top = True
+    save_as = True
+
+
+admin.site.site_title = 'Social Network'
+admin.site.site_header = 'Social Network'

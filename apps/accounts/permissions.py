49d31050dd55fb25216f0af6a6f12657b522654e
Mykhailo Yakubets <mikhail.yakubets@gmail.com>
2023-04-24 17:30:00 +0300

    Add IsCurrentUser permission to UserActivityView

--- apps/accounts/permissions.py
+++ apps/accounts/permissions.py
@@ -3,6 +3,15 @@ from rest_framework.request import Request
 from rest_framework.views import APIView
 
 
+class IsCurrentUser(BasePermission):
+    """
+    Allows access to user who is user from url user_pk.
+    """
+
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        return request.user.id == view.kwargs['pk']
+
+
 class IsStaff(BasePermission):
 
     def has_permission(self, request: Request, view: APIView) -> bool:
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -3,14 +3,14 @@ from rest_framework import generics
 
 from django.contrib.auth import get_user_model
 
-from .permissions import IsStaff
+from .permissions import IsCurrentUser, IsStaff
 from .serializers import UserActivitySerializer
 
 User = get_user_model()
 
 
 class UserActivityView(generics.RetrieveAPIView):
-    permission_classes = [IsAuthenticated | IsStaff]
+    permission_classes = [IsAuthenticated, IsCurrentUser | IsStaff]
     serializer_class = UserActivitySerializer
     queryset = User.objects
 

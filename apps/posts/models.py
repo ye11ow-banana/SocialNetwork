58c5c702154d21261057ac173802d7f82d9aa8b6
Mykhailo Yakubets <mikhail.yakubets@gmail.com>
2023-04-24 07:19:49 +0300

    Add `date_created` field to `PostLike` model

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -69,6 +69,8 @@ class PostLike(models.Model):
         related_name='likes',
         verbose_name='Like owner',
     )
+    date_created = models.DateTimeField(
+        'Post like creation date', auto_now_add=True)
 
     class Meta:
         db_table = 'like'
